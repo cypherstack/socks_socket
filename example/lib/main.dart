f757fa3e5171aaa7ff21b8f84445034177610a28
sneurlax <sneurlax@gmail.com>
2025-01-21 11:55:35 -0600

    feat: use socks_socket package in example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,10 +5,10 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
+import 'package:socks_socket/socks_socket.dart';
 // Imports needed for tor usage:
 import 'package:socks5_proxy/socks_client.dart'; // Just for example; can use any socks5 proxy package, pick your favorite.
 import 'package:tor_ffi_plugin/tor_ffi_plugin.dart';
-import 'package:tor_ffi_plugin/socks_socket.dart'; // For socket connections
 
 void main() {
   runApp(const MyApp());
@@ -77,33 +77,33 @@ class _MyAppState extends State<Home> {
                 onPressed: torIsRunning
                     ? null
                     : () async {
-                  unawaited(
-                    showDialog(
-                      barrierDismissible: false,
-                      context: context,
-                      builder: (_) => const Dialog(
-                        child: Padding(
-                          padding: EdgeInsets.all(20.0),
-                          child: Text("Starting tor..."),
-                        ),
-                      ),
-                    ),
-                  );
+                        unawaited(
+                          showDialog(
+                            barrierDismissible: false,
+                            context: context,
+                            builder: (_) => const Dialog(
+                              child: Padding(
+                                padding: EdgeInsets.all(20.0),
+                                child: Text("Starting tor..."),
+                              ),
+                            ),
+                          ),
+                        );
 
-                  final time = DateTime.now();
+                        final time = DateTime.now();
 
-                  print("NOW: $time");
+                        print("NOW: $time");
 
-                  await startTor();
+                        await startTor();
 
-                  print("Start tor took "
-                      "${DateTime.now().difference(time).inSeconds} "
-                      "seconds");
+                        print("Start tor took "
+                            "${DateTime.now().difference(time).inSeconds} "
+                            "seconds");
 
-                  if (mounted) {
-                    Navigator.of(context).pop();
-                  }
-                },
+                        if (mounted) {
+                          Navigator.of(context).pop();
+                        }
+                      },
                 child: const Text("Start tor"),
               ),
               Row(
@@ -122,38 +122,38 @@ class _MyAppState extends State<Home> {
                   TextButton(
                     onPressed: torIsRunning
                         ? () async {
-                      // `socks5_proxy` package example, use another socks5
-                      // connection of your choice.
+                            // `socks5_proxy` package example, use another socks5
+                            // connection of your choice.
 
-                      // Create HttpClient object
-                      final client = HttpClient();
+                            // Create HttpClient object
+                            final client = HttpClient();
 
-                      // Assign connection factory.
-                      SocksTCPClient.assignToHttpClient(client, [
-                        ProxySettings(InternetAddress.loopbackIPv4,
-                            Tor.instance.port,
-                            password:
-                            null), // TODO Need to get from tor config file.
-                      ]);
+                            // Assign connection factory.
+                            SocksTCPClient.assignToHttpClient(client, [
+                              ProxySettings(InternetAddress.loopbackIPv4,
+                                  Tor.instance.port,
+                                  password:
+                                      null), // TODO Need to get from tor config file.
+                            ]);
 
-                      // GET request.
-                      final request = await client
-                          .getUrl(Uri.parse(hostController.text));
-                      final response = await request.close();
+                            // GET request.
+                            final request = await client
+                                .getUrl(Uri.parse(hostController.text));
+                            final response = await request.close();
 
-                      // Print response.
-                      var responseString =
-                      await utf8.decodeStream(response);
-                      print(responseString);
-                      // If host input left to default icanhazip.com, a Tor
-                      // exit node IP should be printed to the console.
-                      //
-                      // https://check.torproject.org is also good for
-                      // doublechecking torability.
+                            // Print response.
+                            var responseString =
+                                await utf8.decodeStream(response);
+                            print(responseString);
+                            // If host input left to default icanhazip.com, a Tor
+                            // exit node IP should be printed to the console.
+                            //
+                            // https://check.torproject.org is also good for
+                            // doublechecking torability.
 
-                      // Close client
-                      client.close();
-                    }
+                            // Close client
+                            client.close();
+                          }
                         : null,
                     child: const Text("Make proxied request"),
                   ),
@@ -163,54 +163,54 @@ class _MyAppState extends State<Home> {
               TextButton(
                 onPressed: torIsRunning
                     ? () async {
-                  // Instantiate a socks socket at localhost and on the port selected by the tor service.
-                  var socksSocket = await SOCKSSocket.create(
-                    proxyHost: InternetAddress.loopbackIPv4.address,
-                    proxyPort: Tor.instance.port,
-                    sslEnabled: true, // For SSL connections.
-                  );
+                        // Instantiate a socks socket at localhost and on the port selected by the tor service.
+                        var socksSocket = await SOCKSSocket.create(
+                          proxyHost: InternetAddress.loopbackIPv4.address,
+                          proxyPort: Tor.instance.port,
+                          sslEnabled: true, // For SSL connections.
+                        );
 
-                  // Connect to the socks instantiated above.
-                  await socksSocket.connect();
+                        // Connect to the socks instantiated above.
+                        await socksSocket.connect();
 
-                  // Connect to bitcoin.stackwallet.com on port 50002 via socks socket.
-                  //
-                  // Note that this is an SSL example.
-                  await socksSocket.connectTo(
-                      'bitcoin.stackwallet.com', 50002);
+                        // Connect to bitcoin.stackwallet.com on port 50002 via socks socket.
+                        //
+                        // Note that this is an SSL example.
+                        await socksSocket.connectTo(
+                            'bitcoin.stackwallet.com', 50002);
 
-                  // Send a server features command to the connected socket, see method for more specific usage example..
-                  await socksSocket.sendServerFeaturesCommand();
+                        // Send a server features command to the connected socket, see method for more specific usage example..
+                        await socksSocket.sendServerFeaturesCommand();
 
-                  // You should see a server response printed to the console.
-                  //
-                  // Example response:
-                  // `flutter: secure responseData: {
-                  // 	"id": "0",
-                  // 	"jsonrpc": "2.0",
-                  // 	"result": {
-                  // 		"cashtokens": true,
-                  // 		"dsproof": true,
-                  // 		"genesis_hash": "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-                  // 		"hash_function": "sha256",
-                  // 		"hosts": {
-                  // 			"bitcoin.stackwallet.com": {
-                  // 				"ssl_port": 50002,
-                  // 				"tcp_port": 50001,
-                  // 				"ws_port": 50003,
-                  // 				"wss_port": 50004
-                  // 			}
-                  // 		},
-                  // 		"protocol_max": "1.5",
-                  // 		"protocol_min": "1.4",
-                  // 		"pruning": null,
-                  // 		"server_version": "Fulcrum 1.9.1"
-                  // 	}
-                  // }
+                        // You should see a server response printed to the console.
+                        //
+                        // Example response:
+                        // `flutter: secure responseData: {
+                        // 	"id": "0",
+                        // 	"jsonrpc": "2.0",
+                        // 	"result": {
+                        // 		"cashtokens": true,
+                        // 		"dsproof": true,
+                        // 		"genesis_hash": "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
+                        // 		"hash_function": "sha256",
+                        // 		"hosts": {
+                        // 			"bitcoin.stackwallet.com": {
+                        // 				"ssl_port": 50002,
+                        // 				"tcp_port": 50001,
+                        // 				"ws_port": 50003,
+                        // 				"wss_port": 50004
+                        // 			}
+                        // 		},
+                        // 		"protocol_max": "1.5",
+                        // 		"protocol_min": "1.4",
+                        // 		"pruning": null,
+                        // 		"server_version": "Fulcrum 1.9.1"
+                        // 	}
+                        // }
 
-                  // Close the socket.
-                  await socksSocket.close();
-                }
+                        // Close the socket.
+                        await socksSocket.close();
+                      }
                     : null,
                 child: const Text(
                   "Connect to bitcoin.stackwallet.com:50002 (SSL) via socks socket",
